c9438da62cbf7b8689699a923bd27b35a1c80fc1
Tomás Merencio <merencio.tomas@mercadolibre.com>
2021-01-22 15:25:18 -0300

    Update Fecha.java

--- src/EjerciciosMañana/Ejercicio5/Fecha.java
+++ src/EjerciciosMañana/Ejercicio5/Fecha.java
@@ -23,7 +23,10 @@ public class Fecha {
     }
 
     public boolean esCorrecta() {
-        return this.gregorianCalendar.getTime().equals(Calendar.getInstance().getTime());
+        boolean mismoAnio = this.gregorianCalendar.get(Calendar.YEAR) == Calendar.getInstance().get(Calendar.YEAR);
+        boolean mismoMes = this.gregorianCalendar.get(Calendar.MONTH) == Calendar.getInstance().get(Calendar.MONTH);
+        boolean mismoDia = this.gregorianCalendar.get(Calendar.DATE) == Calendar.getInstance().get(Calendar.DATE);
+        return mismoAnio && mismoMes && mismoDia;
     }
 
     public void sumarUnDia(){
@@ -33,7 +36,19 @@ public class Fecha {
     @Override
     public String toString() {
         return this.gregorianCalendar.get(Calendar.YEAR) + "/" +
-                this.gregorianCalendar.get(Calendar.MONTH) + "/" +
+                (this.gregorianCalendar.get(Calendar.MONTH) + 1) + "/" +
                 this.gregorianCalendar.get(Calendar.DATE);
+    }
+
+    public static void main(String[] args) {
+        Fecha fecha = new Fecha(2021, 0, 22);
+
+        System.out.println(fecha.toString());
+
+        System.out.println("Es correcta? " + fecha.esCorrecta());
+
+        fecha.sumarUnDia();
+
+        System.out.println(fecha.toString());
     }
 }
